196768e88a9578256d50e5a36dd070987654fcb5
PAWAN KHIANI <pawan.khiani1998@gmail.com>
2022-03-20 09:50:27 +0500

    ASSIGNMENT 4

---
+++ main.dart
@@ -0,0 +1,64 @@
+//ASSIGNMENT # 04
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  //QUESTION #01:
+  /*final List<dynamic> name_list = [
+    "Bilal",
+    "Bilal",
+    "Bilal",
+    "Owais",
+    "Owais",
+    "Owais"
+  ];
+  final duplicateStrings = name_list.toSet().toList();
+  print(duplicateStrings);
+*/
+
+  //QUESTION # 02
+
+  /*List<dynamic> n = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  var even = n.where((n) => n / 2);
+  print(even);*/
+
+//QUESTION # 03
+/*
+  print('Enter number to check prime or no');
+
+  int? num = int.parse(stdin.readLineSync()!);
+
+  print('$num');
+
+  if (checkPrime(num)) {
+    print('$num is a prime');
+  } else {
+    print('$num is not a prime');
+  }
+}
+
+bool checkPrime(int num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (int i = 2; i <= sqrt(num); i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+*/
+
+// QUESTION # 04
+  /*for (var a = 1; a <= 15; a++) {
+    print(a * 7);
+  }*/
+
+//QUESTION # 05
+
+//QUESTION # 06
+  /* for (var a = 5; a <= 100; a = a + 5) {
+    print(a);
+  }*/
+}
